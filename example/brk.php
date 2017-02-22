6a853df8e720429f731fc9ab168d19947192bab6
Umisagi <zenchiter@gmail.com>
2017-02-22 18:46:54 +0700

    Update brk.php

--- example/brk.php
+++ example/brk.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-<ul>
 <?php
 require_once "../src/WordBreaker.php";
 
@@ -11,6 +7,3 @@ foreach($wordBreaker->breakIntoWords("ฉัน eat ข้าวชิมิ") a
 	print "<li>$w</li>\n";
 }
 ?>
-</ul>
-</body>
-</html>
