7ded342ff6c61d932460ba4fdf871a0dceafdeea
Umisagi <zenchiter@gmail.com>
2017-02-22 18:53:11 +0700

    Update brk.php

--- example/brk.php
+++ example/brk.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../src/WordBreaker.php";
 
-use PhlongTaIam\WordBreaker as WordBreaker;
+#use PhlongTaIam\WordBreaker as WordBreaker;
 $wordBreaker = new WordBreaker("../data/tdict-std.txt");
 foreach($wordBreaker->breakIntoWords("ฉัน eat ข้าวชิมิ") as $w) {
 	print "<li>$w</li>\n";
